de99c310dca81f21ba0a8ee8d2089fa0b60d46b2
Sergio Beppu <beppu.sergio@gmail.com>
2023-04-13 10:12:46 -0300

    POST /participants: todas os requisitos mínimos implementados

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
+import dayjs from "dayjs"
 
 const app = express()
 app.use(express.json())
@@ -13,6 +14,36 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL)
 mongoClient.connect()
 .then(()=> db = mongoClient.db())
 .catch(err => console.log(err.message))
+
+
+app.post("/participants", (req, res) => {
+    const {name} = req.body
+
+    if(!name) {
+        return res.sendStatus(422)
+    }
+
+    const newParticipant = {name: name, lastStatus: Date.now()}
+
+    db.collection("participants").findOne({name: name})
+    .then((data)=>{
+        if(data) {
+            return res.sendStatus(409)
+        } else {
+            const now = dayjs().format("HH:mm:ss")
+            const loginMessage = {from: name, to:"Todos", text:"entra na sala...", type:"status", time: now}
+            db.collection("participants").insertOne(newParticipant)
+            .then(()=> {
+                db.collection("messages").insertOne(loginMessage)
+                .then(res.sendStatus(201))
+                .catch(err => console.log(err.message))
+            })
+            .catch(err => console.log(err.message))
+            
+        }
+    })
+    .catch(err=>console.log(err.message))
+})
 
 
 const PORT = 5000
